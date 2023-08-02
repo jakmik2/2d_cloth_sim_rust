30a5920aa5eea0129a8187905966ebd23754b13c
jakmik2 <40475031+jakmik2@users.noreply.github.com>
2023-08-01 23:41:41 -0400

    Implement ParticleCollection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 pub mod types;
 pub mod euler;
 
+use std::collections::HashMap;
+
 use macroquad::prelude::*;
-use types::{particle::Particle, vector2::Vector2, stick::Stick};
+use types::{particle::*, vector2::Vector2, stick::Stick};
 
 
 #[macroquad::main("2d Cloth Simulation")]
@@ -10,52 +12,66 @@ async fn main() {
     // Make Particles
     let p_a = Particle::with_mass(Vector2 {x: 220., y: 20.}, 1.);
     let p_b = Particle::with_mass(Vector2 {x: 280., y: 20.}, 1.);
-    // let p_c = Particle::with_mass(Vector2 {x: 220., y: 60.}, 1.);
-    // let p_d = Particle::with_mass(Vector2 {x: 280., y: 60.}, 1.);
+    let p_c = Particle::with_mass(Vector2 {x: 280., y: 60.}, 1.);
+    let p_d = Particle::with_mass(Vector2 {x: 220., y: 60.}, 1.);
     
+    // Make Map
+    let mut particle_map = HashMap::new();
 
-    // Add four Particles
-    let mut particles: Vec<Particle> = vec![
-        p_a,
-        p_b,
-        // p_c,
-        // p_d
-    ];
+    particle_map.insert("p_a", p_a);
+    particle_map.insert("p_b", p_b);
+    particle_map.insert("p_c", p_c);
+    particle_map.insert("p_d", p_d);
+
+    // Add Particles
+    let mut particles: ParticleCollection = ParticleCollection::new(particle_map);
 
     // Add four sticks
-    let mut sticks: Vec<Stick> = vec![
-        Stick::new(p_a, p_b),
-        // Stick::new(p_b, p_c),
-        // Stick::new(p_c, p_d),
-        // Stick::new(p_d, p_a),
+    let sticks: Vec<Stick> = vec![
+        Stick::new("p_a", "p_b", p_a.position.get_distance_to(p_b.position)),
+        Stick::new("p_b", "p_c", p_b.position.get_distance_to(p_c.position)),
+        Stick::new("p_c", "p_d", p_c.position.get_distance_to(p_d.position)),
+        Stick::new("p_d", "p_a", p_d.position.get_distance_to(p_a.position)),
+        Stick::new("p_a", "p_c", p_a.position.get_distance_to(p_c.position)),
+        Stick::new("p_b", "p_d", p_b.position.get_distance_to(p_d.position)),
     ];
     
-    draw(&particles);
+    draw(&particles, &sticks);
 
     loop {
         // Verlet Integration
         clear_background(LIGHTGRAY);
 
-        update(&mut particles, &mut sticks);
+        update(&mut particles, &sticks);
 
-        draw(&particles);
+        draw(&particles, &sticks);
 
         next_frame().await
     }
 }
 
 
-fn draw(particles: &Vec<Particle>) {
-    for particle in particles {
-        draw_circle(particle.position.x, particle.position.y, 10., YELLOW)
+fn draw(particles: &ParticleCollection, sticks: &Vec<Stick>) {
+    for key in particles.keys() {
+        let particle_pos = particles.get_position(key);
+        draw_circle(particle_pos.x, particle_pos.y, 10., BLACK)
+    }
+
+    for stick in sticks {
+        let p1 = particles.get_position(stick.p1.as_str());
+        let p2 = particles.get_position(stick.p2.as_str());
+        
+        draw_line(p1.x, p1.y, p2.x, p2.y, 1., WHITE)
     }
 }
 
 
-fn update(particles: &mut Vec<Particle>, sticks: &mut Vec<Stick>) {
+fn update(particles: &mut ParticleCollection, sticks: &Vec<Stick>) {
     let delta_time = get_frame_time();
 
-    for particle in particles {
+    for key in particles.clone().keys() {
+        let mut particle = particles.get_particle(&key);
+
         let force: Vector2 = Vector2 {x: 0., y: 2000.};
         
         let acceleration = Vector2 {x: force.x / particle.mass, y: force.y / particle.mass};
@@ -84,13 +100,22 @@ fn update(particles: &mut Vec<Particle>, sticks: &mut Vec<Stick>) {
         } else if particle.position.x < 0. {
             particle.position.x = 0.;
         }
-
-        // particle.travel_by_verlet(delta_time, acceleration);
-        println!("{:?} PARTICLE", particle.position)
+        
+        particles.set_particle(key, particle);
     }
 
     for stick in sticks {
-        println!("{:?} STICK", stick.p1.position);
-        println!("--------------");
+        let mut p1 = particles.get_particle(stick.p1.as_str());
+        let mut p2 = particles.get_particle(stick.p2.as_str());
+        
+        let diff = p1.position - p2.position;
+        let diff_factor = (stick.length - diff.get_length()) / diff.get_length() * 0.5;
+        let offset: Vector2 = diff * diff_factor;
+
+        p1.position += offset;
+        p2.position -= offset;
+
+        particles.set_particle(stick.p1.as_str(), p1);
+        particles.set_particle(stick.p2.as_str(), p2);
     }
 }
